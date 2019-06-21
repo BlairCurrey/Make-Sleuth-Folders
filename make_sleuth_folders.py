b8a6ad81ef18bde689961381618fffaaa2631993
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2019-06-21 10:54:30 -0400

    Clean-up
    Removed spaces, added "if __name__ == '__main__':" to make it play nicer as a module, and renamed file

--- make_sleuth_folders.py
+++ make_sleuth_folders.py
@@ -7,21 +7,21 @@ print("Files are created in the working directory.")
 print("Which folders do you want to create?" )
 
 # prompt user for input and repeat if invalid
-answer = 0 
+answer = 0
 validInputs = ["a", "r", "p", "x"]
-while answer not in validInputs: 
+while answer not in validInputs:
 	answer = input("(A)ll (R)ookie (P)ro, or e(X)it: ")
 	answer = answer.lower()
 
 # Create rookie folder function
-def create_rookie():	
-	
+def create_rookie():
+
 	# create arrays for parent folders and sub folders
 	rookieParent = [101, 201, 202, 301, 302, 303, 401, 402, 403]
 	rookieSub = [1, 2, 3, 4]
 
 	# loop for creating 9 parent folders, each 4 with sub folders
-	for i in range(0, len(rookieParent), 1):	
+	for i in range(0, len(rookieParent), 1):
 		rookieParentPath = currentPath + "/%s" % rookieParent[i]
 		os.mkdir(rookieParentPath)
 
@@ -31,30 +31,31 @@ def create_rookie():
 
 # Create pro folder function
 def create_pro():
-	
+
 	# create arrays for parent folders and sub folders
 	proParent = [501, 502, 601, 701, 702, 801, 802]
 	proSub = [1, 2, 3, 4]
 
 	# loop for creating 7 parent folders, each 4 with sub folders
-	for i in range(0, len(proParent), 1):	
+	for i in range(0, len(proParent), 1):
 		proParentPath = currentPath + "/%s" % proParent[i]
 		os.mkdir(proParentPath)
 
 		for j in range(0, len(proSub), 1):
 			proSubPath = proParentPath + "/%s" % proSub[j]
-			os.mkdir(proSubPath)	
+			os.mkdir(proSubPath)
 
 # execute functions according to user input
-if answer == "a":
-	create_rookie()
-	create_pro()
-	print("Complete")
-elif answer == "r":
-	create_rookie()
-	print("Complete")
-elif answer == "p":
-	create_pro()
-	print("Complete")
-elif answer == "x":
-	exit()
+if __name__ == '__main__':
+	if answer == "a":
+		create_rookie()
+		create_pro()
+		print("Complete")
+	elif answer == "r":
+		create_rookie()
+		print("Complete")
+	elif answer == "p":
+		create_pro()
+		print("Complete")
+	elif answer == "x":
+		exit()
