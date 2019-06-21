a9d7603e68bd407b70f63671106c07f4463431c8
Sébastien Lavoie <sgdlavoie@gmail.com>
2019-06-21 12:00:21 -0500

    Refactor code to adhere to modern standards
    - Those changes improve readability by simplifying loops and keeping
      everything inside functions, except for global variables
    - Create function `create_structure()` to regroup all execution
    - Add module string at the top of the file
    - Rename global variables to be uppercase according to PEP8 style guide
    - Convert comments to docstrings inside functions
    - Remove check for letter 'x' as this is the last possible case
    - Change percent formatting to `.format()`, which is more modern
    - Rename local variables to be more descriptive
    
    - If the target audience uses Python 3.6 or above, it would be neat to
      use f-strings instead of .format() for string formatting as it is
      gaining in popularity and is arguably easier to read and use

--- make_sleuth_folders.py
+++ make_sleuth_folders.py
@@ -1,61 +1,76 @@
+"""This utility creates directories for the Sleuth files for University
+of London's 'Intro to Programming I' course.
+
+Run the program in the folder where you want to create the directories.
+You can create all the directories, or just the rookie/pro stages."""
+
 import os
 
 # get current directory
-currentPath = os.getcwd()
+CURRENT_PATH = os.getcwd()
+SUB_PATHS = [1, 2, 3, 4]
 
-print("Files are created in the working directory.")
-print("Which folders do you want to create?" )
 
-# prompt user for input and repeat if invalid
-answer = 0
-validInputs = ["a", "r", "p", "x"]
-while answer not in validInputs:
-	answer = input("(A)ll (R)ookie (P)ro, or e(X)it: ")
-	answer = answer.lower()
+def create_structure():
+    """Create directories and sub-directories required for rookie and
+    pro stages of Sleuth cases."""
 
-# Create rookie folder function
+    print("Files are created in the working directory.")
+    print("Which folders do you want to create?")
+
+    # prompt user for input and repeat if invalid
+    answer = None
+    valid_inputs = ["a", "r", "p", "x"]
+    while answer not in valid_inputs:
+        answer = input("(A)ll (R)ookie (P)ro, or e(X)it: ")
+        answer = answer.lower()
+
+    if answer == "a":
+        create_rookie()
+        create_pro()
+        print("Complete")
+    elif answer == "r":
+        create_rookie()
+        print("Complete")
+    elif answer == "p":
+        create_pro()
+        print("Complete")
+    else:
+        exit()
+
+
 def create_rookie():
+    """Create rookie folder function."""
 
-	# create arrays for parent folders and sub folders
-	rookieParent = [101, 201, 202, 301, 302, 303, 401, 402, 403]
-	rookieSub = [1, 2, 3, 4]
+    # create arrays for parent folders and sub folders
+    parent_paths = [101, 201, 202, 301, 302, 303, 401, 402, 403]
 
-	# loop for creating 9 parent folders, each 4 with sub folders
-	for i in range(0, len(rookieParent), 1):
-		rookieParentPath = currentPath + "/%s" % rookieParent[i]
-		os.mkdir(rookieParentPath)
+    # loop for creating 9 parent folders, each 4 with sub folders
+    for directory in parent_paths:
+        new_parent_path = CURRENT_PATH + "/{}".format(directory)
+        os.mkdir(new_parent_path)
 
-		for j in range(0, len(rookieSub), 1):
-			rookieSubPath = rookieParentPath + "/%s" % rookieSub[j]
-			os.mkdir(rookieSubPath)
+        for sub_directory in SUB_PATHS:
+            new_sub_path = new_parent_path + "/{}".format(sub_directory)
+            os.mkdir(new_sub_path)
 
-# Create pro folder function
+
 def create_pro():
+    """Create pro folder function"""
 
-	# create arrays for parent folders and sub folders
-	proParent = [501, 502, 601, 701, 702, 801, 802]
-	proSub = [1, 2, 3, 4]
+    # create arrays for parent folders and sub folders
+    parent_paths = [501, 502, 601, 701, 702, 801, 802]
 
-	# loop for creating 7 parent folders, each 4 with sub folders
-	for i in range(0, len(proParent), 1):
-		proParentPath = currentPath + "/%s" % proParent[i]
-		os.mkdir(proParentPath)
+    # loop for creating 7 parent folders, each 4 with sub folders
+    for directory in parent_paths:
+        new_parent_path = CURRENT_PATH + "/{}".format(directory)
+        os.mkdir(new_parent_path)
 
-		for j in range(0, len(proSub), 1):
-			proSubPath = proParentPath + "/%s" % proSub[j]
-			os.mkdir(proSubPath)
+        for sub_directory in SUB_PATHS:
+            new_sub_path = new_parent_path + "/{}".format(sub_directory)
+            os.mkdir(new_sub_path)
+
 
 # execute functions according to user input
-if __name__ == '__main__':
-	if answer == "a":
-		create_rookie()
-		create_pro()
-		print("Complete")
-	elif answer == "r":
-		create_rookie()
-		print("Complete")
-	elif answer == "p":
-		create_pro()
-		print("Complete")
-	elif answer == "x":
-		exit()
+if __name__ == "__main__":
+    create_structure()
